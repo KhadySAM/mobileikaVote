2d7064c8a475cf389b51dfe88bf1c2f61e656176
Kadidiatou SAMAKE <ksamake18@gmail.com>
2023-02-01 09:43:45 +0000

    init

---
+++ mobileikVote/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobileikVote',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
